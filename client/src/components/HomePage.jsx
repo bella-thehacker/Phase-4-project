8cd1a64f7074b555dc64769be80efdf3bfdc963f
Joshua Nyambane <nyamsjosh@gmail.com>
2024-10-20 11:02:20 +0300

    Start homepage creation

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,21 +1,94 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import videohome from '../assets/videohome.mp4'
 import '../css/HomePage.css'
+import '../css/Navbar.css'
 function HomePage() {
   return (
     <>
       <div>
-        <video autoPlay loop muted className=''>
-          <source src={videohome} type='video/mp4' />
-        </video>
+        <video src={videohome} autoPlay loop muted />
+        <div className='video-content'>
+          <div className='video-content-top'>
+            <p>EN</p>
+            <img src='/Vista.png' alt='' />
+            <NavLink to='/book' className='btn'>
+              BOOK NOW
+            </NavLink>
+          </div>
+          <div className='video-content-bottom'>
+            <div className='navbar_buttons'>
+              <NavLink to='/stay' className='btn'>
+                STAY
+              </NavLink>
+              <NavLink to='/dining' className='btn'>
+                DINING
+              </NavLink>
+              <NavLink to='/spa' className='btn'>
+                SPA
+              </NavLink>
+              <NavLink to='/corporate' className='btn'>
+                CORPORATE
+              </NavLink>
+              <NavLink to='/services' className='btn'>
+                SERVICES
+              </NavLink>
+              <NavLink to='/contact' className='btn'>
+                CONTACT US
+              </NavLink>
+            </div>
+            <img src="/vistaa2.png" alt="" className='h-1/2 w-60'/>
+          </div>
+        </div>
       </div>
       <div className='h-screen w-full'></div>
-      <div className='h-80 w-full bg-yellow-400 my-0 flex justify-center items-center'>
-        <p className='text-4xl text-black w-1/2 h-3/4 my-9 bg-white flex items-center font-serif font-semibold'>
-          A warm welcome to Vista Resort. A luxury five Star boutique resort in
-          Nairobi. We pride ourselves on providing exceptional guest experiences
-          and service, ensuring the utmost comfort and luxury.
+      <div className='h-100 w-full bg-white my-0 flex justify-center items-center'>
+        <p className='text-4xl text-black w-3/4 h-3/4 my-5 py-7 bg-white flex items-center font-serif font-semibold '>
+          <img src='/public/vistaa2.png' alt='' className='h-70 w-60 mr-9' />
+          <div className='w-3 h-3/4 bg-black'></div>A warm welcome to Vista
+          Resort. A luxury five Star boutique resort in Nairobi. We pride
+          ourselves on providing exceptional guest experiences and service,
+          ensuring the utmost comfort and luxury.A leafy oasis where city luxury
+          meets resort allure. Spread across eight acres, indulge in exceptional
+          facilities, exquisite dining, serene gardens and impeccable service.
+          Welcome to our Hotel in Nairobi.
         </p>
+      </div>
+      <div className='stay_homepage'>
+        <h1>Rooms & Suites</h1>
+        <p>
+          Lofty, expansive rooms with vast, bright en-suite bathrooms and large
+          private terraces are characteristic of the hotel’s wonderful sense of
+          space and use of natural light. There are 45 high-ceilinged suites,
+          two of which are penthouse style Presidential Suites and 43 Junior
+          Suites culminating in vaulted beams. Muted colors and textured fabrics
+          in ecru and taupe create an ambiance of contemporary-chic while subtle
+          touches of brass, mahogany and leather bear reference to the classic
+          elegance of vintage East African safaris Beyond the balustrade of your
+          private balcony, we’ve chosen for you a view of The Green Hills of
+          Africa.
+        </p>
+        <div className='w-15 px-5 rounded-xl mt-12 bg-zinc-700'>
+          <NavLink to='/stay' className='view_more2'>
+            View More
+          </NavLink>
+        </div>
+        <div className='img_carousel'></div>
+      </div>
+      <div className='dining_image'>
+        <div className='dining_homepage'></div>
+        <div className='dining_homepage_inner'>
+          <h1>Dining</h1>
+          <p>
+            Enjoy an array of international dining experiences that will satisfy
+            the most refined palettes in Nairobi.
+          </p>
+          <div className='w-15 px-5 rounded-xl mt-12 bg-zinc-700'>
+            <NavLink to='/dining' className='view_more'>
+              View More
+            </NavLink>
+          </div>
+        </div>
       </div>
       <div className='text-2xl'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -55,23 +55,6 @@ function Navbar() {
           <NavLink to='/contact' className='btn'>
             CONTACT US
           </NavLink>
-
-
-          <FlyoutLink href='/services' className='btn' FlyoutContent={PricingContent}>
-            Services
-          </FlyoutLink>
-
-
-          <NavLink to='/book' className='btn'>
-
-          <a href='/contact' className='btn'>
-            CONTACT US
-          </a>
-          <a href='/book' className='btn'></a>
-
-            BOOK NOW
-          </NavLink>
-
         </div>
       </nav>
       <div className={menu_class}>
@@ -102,59 +85,8 @@ function Navbar() {
             className='burger_image4'></motion.div>
         </div>
       </div>
-      {/* <div className="flyout">
-        <FlyoutLink
-            href='/services'
-            className='btn'
-            FlyoutContent={PricingContent}>
-            Services
-          </FlyoutLink>
-      </div> */}
     </>
   )
-}
-
-const FlyoutLink = ({ children, href, FlyoutContent }) => {
-  const [open, setOpen] = useState(false)
-
-  const showFlyout = open && FlyoutContent
-
-  return (
-    <div
-      onMouseEnter={() => setOpen(true)}
-      onMouseLeave={() => setOpen(false)}
-      className='relative h-fit w-fit'>
-      <a href={href} className='relative text-2xl text-white'>
-        {children}
-        <span
-          style={{
-            transform: showFlyout ? 'scaleX(1)' : 'scaleX(0)',
-          }}
-          className='absolute -bottom-1 -left-2 -right-2 h-0.5 origin-left rounded-full bg-white transition-transform duration-300 ease-out'
-        />
-      </a>
-      <AnimatePresence>
-        {showFlyout && (
-          <motion.div
-            initial={{ opacity: 0, y: 15 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 15 }}
-            style={{
-              translateX: '-50%',
-            }}
-            className='absolute left-1/2 top-12 bg-white text-black '>
-            <div className='absolute -top-6 left-0 right-0 h-6 bg-transparent'></div>
-            <div className='absolute left-1/2 top-0.5 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white'></div>
-            <FlyoutContent />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  )
-}
-
-const PricingContent = () => {
-  return <div className='h-24 w-64  bg-white p-6 shadow-xl'></div>
 }
 
 export default Navbar
