16cebc711d19748c3367b14837ec1b6b07ef2061
Joshua Nyambane <165890257+jNyambane@users.noreply.github.com>
2024-10-28 11:56:52 +0300

    Update Contact.jsx

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -5,9 +5,9 @@ import ReviewForm from "./ReviewForm";
 const Contact = () => {
   return (
     <div className="contact-container">
-      <header className="contact-header">
-        <h1>Contact Us</h1>
-        <p>
+      <header className="w-[50%] h-auto object-contain text-center">
+        <h1 className=" text-[2rem]">Contact Us</h1>
+        <p className="text-[1.8]">
           We’d love to hear from you! Please fill out the form below or contact
           us directly.
         </p>
@@ -24,17 +24,17 @@ const Contact = () => {
             Nairobi, Kenya
           </p>
           <h3>Phone</h3>
-          <p>+254 (0) 709 195 256</p>
+          <p>+254 (0) 787 654 321</p>
           <h3>Email</h3>
           <p>
-            <a href="mailto:sales@hemingways.co">sales@hemingways.co</a>
+            <a href="mailto:sales@hemingways.co">sales@vistaresort.co</a>
           </p>
         </div>
 
         <div className="map-container">
           <h2>Find Us Here</h2>
           <iframe
-            title="Hemingways Nairobi Map"
+            title="Vista REsort Nairobi Map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.196047171162!2d36.79805541530063!3d-1.3494848990251546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10c06b0b11d7%3A0x8d929bd8e40dcbf8!2sHemingways%20Nairobi!5e0!3m2!1sen!2ske!4v1631267801230!5m2!1sen!2ske"
             width="600"
             height="450"
